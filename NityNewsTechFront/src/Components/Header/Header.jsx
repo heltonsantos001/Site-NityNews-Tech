c63550005ec9ec85e8127ac8a61a2e0dc4c0b394
Helton Santos <heltonsanto001@gmail.com>
2024-12-06 23:09:26 -0300

    responsividade

--- NityNewsTechFront/src/Components/Header/Header.jsx
+++ NityNewsTechFront/src/Components/Header/Header.jsx
@@ -1,14 +1,30 @@
 import { MenuBar } from "../Menu/MenuBar";
 import logo from "./../../Img/Logo.png"
 import "./HeaderStyle.css"
+import { WidthFunction } from "../../Function/WidthFunction";
+import { useState, useEffect } from "react";
 
 
 
 export function Header(){
+
+    const [MenuView, setMenuView] = useState(true);
+    const width = WidthFunction();
+
+    function seeMenu(){
+        if(width < 945) return setMenuView(false)
+            setMenuView(true)
+    }
+      useEffect(() => {
+
+        seeMenu()
+
+      }, [width]);
+
     return(
         <header>
             <img src={logo} alt="" />
-            <MenuBar />
+            {MenuView && <MenuBar />}
             <button>
                 Sign up
                 <div class="arrow-wrapper">
--- NityNewsTechFront/src/Components/Main/Main.jsx
+++ NityNewsTechFront/src/Components/Main/Main.jsx
@@ -5,18 +5,33 @@ import "./MainStyle.css"
 import { Letter } from "../Letter/Letter"
 import { CardText } from "../CardText/CardText"
 import { CardNews } from "../CardNews/CardNews"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { MenuBar } from "../Menu/MenuBar";
+import { WidthFunction } from "../../Function/WidthFunction";
 
 export function Main(){
+
+    const [MenuView, setMenuView] = useState(false);
+    const width = WidthFunction();
+
+    function seeMenu(){
+        if(width < 945) return setMenuView(true)
+            setMenuView(false)
+    }
 
     useEffect(() => {
         AOS.init(); 
       }, []);
 
+      useEffect(() => {
+        seeMenu()
+
+      }, [width]);
+
+
     return(   
         <main>   
-            {<MenuBar /> && false}
+            {MenuView && <MenuBar /> }
             <div id="ContainerTop">
             <div data-aos-duration="1500" data-aos="fade-up" id="ContainerLetter">
                     <Letter className="text-xl font-bold text-gray-500" />
@@ -35,7 +50,7 @@ export function Main(){
             <div data-aos-duration="1500" data-aos="fade-up" id="ContainerCardText">
                 <h1 className="text-xl font-bold text-gray-500">Seja perseguido, por boas notícias, claro!</h1>
                 <p className="text-xl font-bold text-gray-500">
-                    Já pensou estar sempre atualizado sobre o mundo da tecnologia sem precisar procurar? A gente tem a solução perfeita, inscreva-se e deixe que as informações corram atrás de você. Pode relaxar, a gente garante que é uma perseguição saudável e cheia de novidades incríveis!
+                   {width} Já pensou estar sempre atualizado sobre o mundo da tecnologia sem precisar procurar? A gente tem a solução perfeita, inscreva-se e deixe que as informações corram atrás de você. Pode relaxar, a gente garante que é uma perseguição saudável e cheia de novidades incríveis!
                 </p>    
             </div>
                 
