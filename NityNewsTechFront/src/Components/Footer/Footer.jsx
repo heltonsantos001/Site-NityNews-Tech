0f9088e1a6010b4ceca03f5856e8cf19378a171e
Helton Santos <heltonsanto001@gmail.com>
2024-12-07 19:53:02 -0300

    Footer finalizado

--- NityNewsTechFront/src/Components/Footer/Footer.jsx
+++ NityNewsTechFront/src/Components/Footer/Footer.jsx
@@ -13,12 +13,38 @@ export function Footer() {
         <div className="wave" id="wave3"></div>
         <div className="wave" id="wave4"></div>
       </div>
-     <div id="ContainerMainFooter">
-    
-     </div>
-     
+      <div className="ContainerFooter">
 
+      
+      <div className="ContainerTextFooter">
+      <h1 className="TitleFooter">
+        Megulhe em um mar de novidades e conhecimento sobre a tecnologia
+      </h1>
+      </div>
+      
+     <div id="ContainerMainFooter">
+        <Feedback />
+        <div class="footer-about">
+            <h4>Sobre a NityNews Tech</h4>
+            <p>TechSphere é o seu portal de notícias sobre tecnologia, trazendo atualizações, inovações e tendências do setor.</p>
+        </div>
+        <div class="footer-links">
+            <h4>Links Rápidos</h4>
+            <ul>
+                <li><a href="/sobre">Home</a></li>
+                <li><a href="/sobre">Sobre nós</a></li>
+                <li><a href="/politica-privacidade">Política de Privacidade</a></li>
+                <li><a href="/termos-uso">Termos de Uso</a></li>
+                <li><a href="/contato">Contato</a></li>
+            </ul>
+        </div>
+     </div>
+     <div id="ContainerRedes">
+            <Instagram />
+            <Linkedin />
+        </div>
       <p>&copy;2024 NityNews tech | All Rights Reserved</p>
+      </div>
       <script
         type="module"
         src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
