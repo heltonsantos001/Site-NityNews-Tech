09b4a2efaa89660dd1084411cca4bb0bfec69378
helton santos <heltonsanto001@gmail.com>
2025-02-22 21:44:50 -0300

    remove wave to footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,12 +7,7 @@ import { Instagram } from "../IconRedes/Instagram/Instagram";
 export function Footer() {
   return (
     <footer className="footer">
-      <div className="waves">
-        <div className="wave" id="wave1"></div>
-        <div className="wave" id="wave2"></div>
-        <div className="wave" id="wave3"></div>
-        <div className="wave" id="wave4"></div>
-      </div>
+    
       <div className="ContainerFooter">
 
       
